ac29ef5235ab563a518d605422080e6eee711638
nadav <nadavma@edu.hac.ac.il>
2023-01-12 23:31:31 +0200

    Those cookies are not edible :/

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -1,4 +1,4 @@
-const Account = require('../models/account');
+//const Account = require('../models/account');
 
 const Cookies = require('cookies');
 // the controllers folder allows us to separate the logic from the routes.
@@ -15,15 +15,40 @@ const Cookies = require('cookies');
  */
 const keys=['keyboard cat']
 exports.getRegister = (req, res, next) => {
-    console.log(req.cookie)
-    const account = new Account(req,res);
-    account.save();
-
+    const cookies = new Cookies(req, res, {keys : keys});
+    const user = cookies.get('user');
+    let email =''
+    let firstname = ''
+    let lastname= ''
+    if(user){
+        let indices = findIndices(user.toString(), 'email','firstname','lastname')
+        if(indices){
+            email = indices.email;
+            firstname = indices.firstname;
+            lastname = indices.lastname;
+        }
+    }
     res.render('registration', {
         pageTitle: 'Registration',
-        path: '/admin/register'
+        path: '/admin/register',
+        email : email,
+        firstname: firstname,
+        lastname: lastname
     });
 };
+/*Finding indices of user info from a string*/
+const findIndices = (string, s1,s2,s3)=>{
+    let i = string.search(s1);
+    let j = string.search(s2);
+    let k = string.search(s3);
+    if(i!== -1 && j !== -1 && k!== -1){
+        return { email : string.substring(i + s1.length + 3, j - 3),
+            firstname : string.substring(j + s2.length + 3, k - 3),
+            lastname : string.substring(k + s3.length + 3, string.length - 2)
+        };
+    }
+    return null;
+}
 
 /**
  * handles the post request from the add product page.
@@ -34,8 +59,14 @@ exports.getRegister = (req, res, next) => {
  */
 exports.postRegister = (req, res, next) => {
     try {
-        const account = new Account(req,res);
-        account.save();
+
+        const cookies = new Cookies(req, res, {keys : keys});
+        const email = req.body.email
+        const firstname = req.body.firstname
+        const lastname = req.body.lastname
+        const userData = {email: email, firstname: firstname, lastname: lastname};
+        cookies.set('user', JSON.stringify(userData), {singed: true, maxAge: 30* 1000});
+
         res.redirect('/admin/register-password');
     } catch (err) {
         // TO DO! we must handle the error here and generate a EJS page to display the error.
@@ -49,20 +80,18 @@ exports.postRegister = (req, res, next) => {
  * @param next
  */
 exports.getAccounts = (req, res, next) => {
-    let accounts = Account.fetchAll();
+   // let accounts = Account.fetchAll(); db not implemented yet
     res.render('login', {
-        accounts: accounts,
+        //accounts: accounts,
         pageTitle: 'Login',
         path: '/',
-        hasProducts: accounts.length > 0
+        //hasProducts: accounts.length > 0
     });
 
 };
 
 exports.getRegisterPassword = (req, res, next) => {
    // const cookies = new Cookies(req,res, {keys: keys});
-    const account = new Account(req,res);
-    account.save();
 
     res.render('register-password', {
         pageTitle: 'Registration',
@@ -78,9 +107,9 @@ exports.getRegisterPassword = (req, res, next) => {
  * @param next
  */
 exports.postRegisterPassword = (req, res, next) => {
-    try {
-        const account = new Account(req,res);
-        account.save();
+    try {//HERE WE SHOULD CHECK, IF COOKIE LIFETIME IS DEAD MAYBE REDIRECT TO START
+        //const account = new Account(req,res);
+        //account.save();
         res.redirect('/');
     } catch (err) {
         // TO DO! we must handle the error here and generate a EJS page to display the error.
